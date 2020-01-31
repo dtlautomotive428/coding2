4eebaf5c01378dfdd34d7b2ef5a6205944101e53
dtlautomotive428 <60458707+dtlautomotive428@users.noreply.github.com>
2020-01-31 07:51:21 -0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,25 @@
+// my first attempt not knowing what in the world im doing//
+public class App {
+//first set of two variables//	
+
+	public static void main(String[] args) {
+double Price = 4.99 ;
+System.out.println(4.99);
+double MoneyInWallet = 500.00 ;
+System.out.println(500.00);
+int NOF = 3 ;
+System.out.println(3);
+
+int age = 49 ;
+System.out.println(49);
+String firstName = "Delio" ;
+System.out.println("Delio");
+String lastName = "Lopez" ;
+System.out.println("Lopez");
+char middleIntitial = 'T' ;
+System.out.println('T');
+
+
+	}
+
+}
